41049041a965de45c0f9858e3ac8c43ff14a2344
Saatvik07 <saatvik19097@iiitd.ac.in>
2020-10-25 05:12:11 +0530

    migration added, single script added

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+module.exports = async function main(callback) {
+    try {
+        const Box = artifacts.require("Box");
+        const box = await Box.deployed();
+        value = await box.retrieve();
+        console.log("Box value is", value.toString());
+      callback(0);
+    } catch (error) {
+      console.error(error);
+      callback(1);
+    }
+  }
